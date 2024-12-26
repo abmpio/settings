92787cebb48a7d4ecd45d9eda45a1a4c98a09925
jeseeqing <jeseeqing@gmail.com>
2024-12-26 13:10:02 +0800

    add Tag,Description field in setting model

--- setting.go
+++ setting.go
@@ -22,8 +22,10 @@ const (
 type Setting struct {
 	mongodbr.AuditedEntity `bson:",inline"`
 
-	AppName   string         `json:"appName" bson:"appName"`
-	ValueType ValueFieldType `json:"valueType" bson:"valueType"`
+	AppName     string         `json:"appName" bson:"appName"`
+	ValueType   ValueFieldType `json:"valueType" bson:"valueType"`
+	Tag         string         `json:"tag" bson:"tag"`
+	Description string         `json:"description" bson:"description"`
 	//当值为true时,表示此值是一个受保护的属性值,服务器不会将数据发送到前端
 	ProtectedInUi bool `json:"protectedInUi" bson:"protectedInUi"`
 	NameValue     `bson:",inline"`
